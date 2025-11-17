3aa12d4ac5900cb706939367b0b1ed003cbdca9d
Abhilash Mundepi <160834131+AbhilashMundepi@users.noreply.github.com>
2025-11-17 21:48:13 +0530

    Change root route from '/listings' to '/'

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use((req, res, next) => {
 });
 
 // ============ ROUTES ===================
-app.get("/listings", (req, res) => {
+app.get("/", (req, res) => {
     res.send("hi, I am root — Render Deployment Success 🎉");
 });
 
